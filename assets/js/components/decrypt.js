c682720c2e4a694a1cdf2c88eba6d913d87163a7
Juan Torres <junete190@gmail.com>
2024-08-05 14:49:36 -0500

    Solución errores desencriptación

--- assets/js/components/decrypt.js
+++ assets/js/components/decrypt.js
@@ -1,17 +1,14 @@
 function decrypt() {
-  let text = document.querySelector(".main__input");
+  let text = document.querySelector(".main__input").value;
   let button = document.getElementById("decrypt");
   let copyButton = document.getElementById("copyButton");
 
-  function decryptText(text) {
-    text = text
-      .replace(/ai/g, "a")
-      .replace(/enter/g, "e")
-      .replace(/imes/g, "i")
-      .replace(/ober/g, "o")
-      .replace(/ufat/g, "u");
-    return text;
-  }
+  text = text
+    .replace(/ai/g, "a")
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
 
   let solution = document.getElementById("solution");
   let title = document.querySelector(".solution__title");
@@ -20,16 +17,16 @@ function decrypt() {
     title.style.display = "none";
     solution.style.marginBottom = "10px";
     copyButton.style.display = "block";
+    solution.innerHTML = text
   } else {
     title.style.display = "block";
     solution.style.marginBottom = "20px";
     solution.innerHTML = "Ingresa el texto que desees encriptar o desencriptar";
+    copyButton.style.display = 'none'
   }
-  
+
   button.addEventListener("click", function () {
-    let encryptedText = text.value;
-    let decryptedText = decryptText(encryptedText);
-    solution.innerHTML = decryptedText;
+    decrypt(text);
   });
 }
 
